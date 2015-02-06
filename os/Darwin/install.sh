1d4ff71130f04a7c9e3b658beb72b86b4b7512c0
Ramón Medrano Llamas <m3drano@gmail.com>
2015-02-06 20:33:34 +0100

    Uncomment some of the OS X configs.

--- os/Darwin/install.sh
+++ os/Darwin/install.sh
@@ -35,27 +35,27 @@ unset log
 # #sudo scutil --set LocalHostName "0x6D746873"
 # #sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "0x6D746873"
 
-# # Set standby delay to 24 hours (default is 1 hour)
-# #sudo pmset -a standbydelay 86400
+# Set standby delay to 24 hours (default is 1 hour)
+#sudo pmset -a standbydelay 86400
 
-# # Disable the sound effects on boot
-# sudo nvram SystemAudioVolume=" "
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
 
 # # Menu bar: disable transparency
-# defaults write NSGlobalDomain AppleEnableMenuBarTransparency -bool false
+defaults write NSGlobalDomain AppleEnableMenuBarTransparency -bool false
 
-# # Menu bar: hide the Time Machine, Volume, User, and Bluetooth icons
-# for domain in ~/Library/Preferences/ByHost/com.apple.systemuiserver.*; do
-# 	defaults write "${domain}" dontAutoLoad -array \
-# 		"/System/Library/CoreServices/Menu Extras/TimeMachine.menu" \
-# 		"/System/Library/CoreServices/Menu Extras/Volume.menu" \
-# 		"/System/Library/CoreServices/Menu Extras/User.menu"
-# done
-# defaults write com.apple.systemuiserver menuExtras -array \
-# 	"/System/Library/CoreServices/Menu Extras/Bluetooth.menu" \
-# 	"/System/Library/CoreServices/Menu Extras/AirPort.menu" \
-# 	"/System/Library/CoreServices/Menu Extras/Battery.menu" \
-# 	"/System/Library/CoreServices/Menu Extras/Clock.menu"
+# Menu bar: hide the Time Machine, Volume, User, and Bluetooth icons
+for domain in ~/Library/Preferences/ByHost/com.apple.systemuiserver.*; do
+	defaults write "${domain}" dontAutoLoad -array \
+		"/System/Library/CoreServices/Menu Extras/TimeMachine.menu" \
+		"/System/Library/CoreServices/Menu Extras/Volume.menu" \
+		"/System/Library/CoreServices/Menu Extras/User.menu"
+done
+defaults write com.apple.systemuiserver menuExtras -array \
+	"/System/Library/CoreServices/Menu Extras/Bluetooth.menu" \
+	"/System/Library/CoreServices/Menu Extras/AirPort.menu" \
+	"/System/Library/CoreServices/Menu Extras/Battery.menu" \
+	"/System/Library/CoreServices/Menu Extras/Clock.menu"
 
 # # Set highlight color to green
 # #defaults write NSGlobalDomain AppleHighlightColor -string "0.764700 0.976500 0.568600"
@@ -71,85 +71,85 @@ unset log
 # # (Uncomment if you’re on an older Mac that messes up the animation)
 # #defaults write NSGlobalDomain NSScrollAnimationEnabled -bool false
 
-# # Increase window resize speed for Cocoa applications
-# defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+# Increase window resize speed for Cocoa applications
+defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
 
-# # Expand save panel by default
-# defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-# defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 
-# # Expand print panel by default
-# defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
-# defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+# Expand print panel by default
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
 
-# # Save to disk (not to iCloud) by default
-# defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+# Save to disk (not to iCloud) by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
-# # Automatically quit printer app once the print jobs complete
-# defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
-# # Disable the “Are you sure you want to open this application?” dialog
-# defaults write com.apple.LaunchServices LSQuarantine -bool false
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
 
-# # Display ASCII control characters using caret notation in standard text views
-# # Try e.g. `cd /tmp; unidecode "\x{0000}" > cc.txt; open -e cc.txt`
-# defaults write NSGlobalDomain NSTextShowsControlCharacters -bool true
+# Display ASCII control characters using caret notation in standard text views
+# Try e.g. `cd /tmp; unidecode "\x{0000}" > cc.txt; open -e cc.txt`
+defaults write NSGlobalDomain NSTextShowsControlCharacters -bool true
 
-# # Disable Resume system-wide
-# defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
+# Disable Resume system-wide
+defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
 
-# # Disable automatic termination of inactive apps
-# defaults write NSGlobalDomain NSDisableAutomaticTermination -bool true
+# Disable automatic termination of inactive apps
+defaults write NSGlobalDomain NSDisableAutomaticTermination -bool true
 
-# # Disable the crash reporter
-# defaults write com.apple.CrashReporter DialogType -string "none"
+# Disable the crash reporter
+defaults write com.apple.CrashReporter DialogType -string "none"
 
-# # Set Help Viewer windows to non-floating mode
-# defaults write com.apple.helpviewer DevMode -bool true
+# Set Help Viewer windows to non-floating mode
+defaults write com.apple.helpviewer DevMode -bool true
 
-# # Fix for the ancient UTF-8 bug in QuickLook (http://mths.be/bbo)
-# # Commented out, as this is known to cause problems in various Adobe apps :(
-# # See https://github.com/mathiasbynens/dotfiles/issues/237
-# #echo "0x08000100:0" > ~/.CFUserTextEncoding
+# Fix for the ancient UTF-8 bug in QuickLook (http://mths.be/bbo)
+# Commented out, as this is known to cause problems in various Adobe apps :(
+# See https://github.com/mathiasbynens/dotfiles/issues/237
+echo "0x08000100:0" > ~/.CFUserTextEncoding
 
-# # Reveal IP address, hostname, OS version, etc. when clicking the clock
-# # in the login window
-# sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+# Reveal IP address, hostname, OS version, etc. when clicking the clock
+# in the login window
+sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
 
-# # Restart automatically if the computer freezes
-# systemsetup -setrestartfreeze on
+# Restart automatically if the computer freezes
+systemsetup -setrestartfreeze on
 
 # # Never go into computer sleep mode
-# #systemsetup -setcomputersleep Off > /dev/null
+# # systemsetup -setcomputersleep Off > /dev/null
 
 # # Check for software updates daily, not just once per week
 # #defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
 
-# # Disable Notification Center and remove the menu bar icon
-# launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
+# Disable Notification Center and remove the menu bar icon
+launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
 
-# # Disable smart quotes as they’re annoying when typing code
-# defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+# Disable smart quotes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
 
-# # Disable smart dashes as they’re annoying when typing code
-# defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+# Disable smart dashes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 
 # ###############################################################################
 # # SSD-specific tweaks                                                         #
 # ###############################################################################
 
-# # Disable local Time Machine snapshots
-# sudo tmutil disablelocal
+# Disable local Time Machine snapshots
+sudo tmutil disablelocal
 
-# # Disable hibernation (speeds up entering sleep mode)
-# sudo pmset -a hibernatemode 0
+# Disable hibernation (speeds up entering sleep mode)
+sudo pmset -a hibernatemode 0
 
-# # Remove the sleep image file to save disk space
-# sudo rm /Private/var/vm/sleepimage
-# # Create a zero-byte file instead…
-# sudo touch /Private/var/vm/sleepimage
-# # …and make sure it can’t be rewritten
-# sudo chflags uchg /Private/var/vm/sleepimage
+# Remove the sleep image file to save disk space
+sudo rm /Private/var/vm/sleepimage
+# Create a zero-byte file instead…
+sudo touch /Private/var/vm/sleepimage
+# …and make sure it can’t be rewritten
+sudo chflags uchg /Private/var/vm/sleepimage
 
 # # Disable the sudden motion sensor as it’s not useful for SSDs
 # #sudo pmset -a sms 0
@@ -202,11 +202,11 @@ unset log
 # # Set the timezone; see `systemsetup -listtimezones` for other values
 # #systemsetup -settimezone "Europe/Brussels" > /dev/null
 
-# # Disable auto-correct
-# defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+# Disable auto-correct
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
-# # Stop iTunes from responding to the keyboard media keys
-# launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null
+# Stop iTunes from responding to the keyboard media keys
+launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null
 
 # ###############################################################################
 # # Screen                                                                      #
@@ -216,17 +216,17 @@ unset log
 # defaults write com.apple.screensaver askForPassword -int 1
 # defaults write com.apple.screensaver askForPasswordDelay -int 0
 
-# # Save screenshots to the desktop
-# defaults write com.apple.screencapture location -string "${HOME}/Desktop"
+# Save screenshots to the desktop
+defaults write com.apple.screencapture location -string "${HOME}/Desktop"
 
-# # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
-# defaults write com.apple.screencapture type -string "png"
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
 
-# # Disable shadow in screenshots
-# defaults write com.apple.screencapture disable-shadow -bool true
+# Disable shadow in screenshots
+defaults write com.apple.screencapture disable-shadow -bool true
 
-# # Enable subpixel font rendering on non-Apple LCDs
-# defaults write NSGlobalDomain AppleFontSmoothing -int 2
+# Enable subpixel font rendering on non-Apple LCDs
+defaults write NSGlobalDomain AppleFontSmoothing -int 2
 
 # # Enable HiDPI display modes (requires restart)
 # #sudo defaults write /Library/Preferences/com.apple.windowserver DisplayResolutionEnabled -bool true
@@ -235,8 +235,8 @@ unset log
 # # Finder                                                                      #
 # ###############################################################################
 
-# # Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
-# defaults write com.apple.finder QuitMenuItem -bool true
+# Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
+defaults write com.apple.finder QuitMenuItem -bool true
 
 # # Finder: disable window animations and Get Info animations
 # #defaults write com.apple.finder DisableAllAnimations -bool true
@@ -246,11 +246,11 @@ unset log
 # defaults write com.apple.finder NewWindowTarget -string "PfDe"
 # defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/Desktop/"
 
-# # Show icons for hard drives, servers, and removable media on the desktop
-# defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-# #defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
-# defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
-# defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+# Show icons for hard drives, servers, and removable media on the desktop
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+#defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
 # # Finder: show hidden files by default
 # #defaults write com.apple.finder AppleShowAllFiles -bool true
@@ -258,42 +258,42 @@ unset log
 # # Finder: show all filename extensions
 # defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-# # Finder: show status bar
-# #defaults write com.apple.finder ShowStatusBar -bool true
+# Finder: show status bar
+#defaults write com.apple.finder ShowStatusBar -bool true
 
 # # Finder: show path bar
 # #defaults write com.apple.finder ShowPathbar -bool true
 
-# # Finder: allow text selection in Quick Look
-# defaults write com.apple.finder QLEnableTextSelection -bool true
+# Finder: allow text selection in Quick Look
+defaults write com.apple.finder QLEnableTextSelection -bool true
 
-# # Display full POSIX path as Finder window title
-# defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
-# # When performing a search, search the current folder by default
-# defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
-# # Disable the warning when changing a file extension
-# defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
-# # Enable spring loading for directories
-# defaults write NSGlobalDomain com.apple.springing.enabled -bool true
+# Enable spring loading for directories
+defaults write NSGlobalDomain com.apple.springing.enabled -bool true
 
-# # Remove the spring loading delay for directories
-# defaults write NSGlobalDomain com.apple.springing.delay -float 0
+# Remove the spring loading delay for directories
+defaults write NSGlobalDomain com.apple.springing.delay -float 0
 
-# # Avoid creating .DS_Store files on network volumes
-# defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
-# # Disable disk image verification
-# defaults write com.apple.frameworks.diskimages skip-verify -bool true
-# defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
-# defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
+# Disable disk image verification
+defaults write com.apple.frameworks.diskimages skip-verify -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
 
-# # Automatically open a new Finder window when a volume is mounted
-# #defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
-# #defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
-# #defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
+# Automatically open a new Finder window when a volume is mounted
+#defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+#defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+#defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
 
 # # Show item info near icons on the desktop and in other icon views
 # /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
@@ -323,7 +323,7 @@ unset log
 # defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 
 # # Disable the warning before emptying the Trash
-# defaults write com.apple.finder WarnOnEmptyTrash -bool false
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
 
 # # Empty Trash securely by default
 # #defaults write com.apple.finder EmptyTrashSecurely -bool true
@@ -378,8 +378,8 @@ unset log
 # # Don’t animate opening applications from the Dock
 # #defaults write com.apple.dock launchanim -bool false
 
-# # Speed up Mission Control animations
-# defaults write com.apple.dock expose-animation-duration -float 0.1
+# Speed up Mission Control animations
+defaults write com.apple.dock expose-animation-duration -float 0.1
 
 # # Don’t group windows by application in Mission Control
 # # (i.e. use the old Exposé behavior instead)
@@ -394,19 +394,19 @@ unset log
 # # Don’t automatically rearrange Spaces based on most recent use
 # defaults write com.apple.dock mru-spaces -bool false
 
-# # Remove the auto-hiding Dock delay
-# defaults write com.apple.dock autohide-delay -float 0
-# # Remove the animation when hiding/showing the Dock
-# #defaults write com.apple.dock autohide-time-modifier -float 0
+# Remove the auto-hiding Dock delay
+defaults write com.apple.dock autohide-delay -float 0
+# Remove the animation when hiding/showing the Dock
+#defaults write com.apple.dock autohide-time-modifier -float 0
 
-# # Enable the 2D Dock
-# defaults write com.apple.dock no-glass -bool true
+# Enable the 2D Dock
+defaults write com.apple.dock no-glass -bool true
 
 # # Automatically hide and show the Dock
 # #defaults write com.apple.dock autohide -bool true
 
-# # Make Dock icons of hidden applications translucent
-# defaults write com.apple.dock showhidden -bool true
+# Make Dock icons of hidden applications translucent
+defaults write com.apple.dock showhidden -bool true
 
 # # Make Dock more transparent
 # defaults write com.apple.dock hide-mirror -bool true
@@ -547,8 +547,8 @@ unset log
 # # Terminal & iTerm 2                                                          #
 # ###############################################################################
 
-# # Only use UTF-8 in Terminal.app
-# defaults write com.apple.terminal StringEncodings -array 4
+# Only use UTF-8 in Terminal.app
+defaults write com.apple.terminal StringEncodings -array 4
 
 # # Use a modified version of the Pro theme by default in Terminal.app
 # # open "${HOME}/init/Mathias.terminal"
@@ -571,11 +571,11 @@ unset log
 # # Time Machine                                                                #
 # ###############################################################################
 
-# # Prevent Time Machine from prompting to use new hard drives as backup volume
-# defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+# Prevent Time Machine from prompting to use new hard drives as backup volume
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
-# # Disable local Time Machine backups
-# hash tmutil &> /dev/null && sudo tmutil disablelocal
+# Disable local Time Machine backups
+hash tmutil &> /dev/null && sudo tmutil disablelocal
 
 # ###############################################################################
 # # Activity Monitor                                                            #
