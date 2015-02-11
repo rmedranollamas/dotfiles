ec1e94675ea04cfc6fa82ed8ff18c91edd1b4e3e
Ramón Medrano Llamas <m3drano@gmail.com>
2015-02-11 22:24:19 +0100

    Fix Flymake for LaTeX.

--- emacs/emacs.d.symlink/config/latex-config.el
+++ emacs/emacs.d.symlink/config/latex-config.el
@@ -9,6 +9,10 @@
 ;; For LaTeX, let's use newer options.
 (when (load "flymake" t)
   (defun flymake-get-tex-args (file-name)
-    (list "pdflatex" (list "-file-line-error"
-                           "-draftmode"
-                           "-interaction=nonstopmode" file-name))))
+    (list "/usr/texbin/pdflatex" (list "-file-line-error"
+                                       "-draftmode"
+                                       "-interaction=nonstopmode" file-name))))
+
+;; Set the path for TeXLive
+(setenv "PATH"
+  (concat "/usr/texbin" ":" (getenv "PATH")))
