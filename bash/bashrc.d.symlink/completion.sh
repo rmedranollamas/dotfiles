5e52fd7f494decf3ccb7fe32fc590c8bff002a58
Ramón Medrano Llamas <m3drano@gmail.com>
2015-02-11 20:12:46 +0100

    Move Hombrew completion sourcing to platform dir.

--- bash/bashrc.d.symlink/completion.sh
+++ bash/bashrc.d.symlink/completion.sh
@@ -4,16 +4,8 @@
 
 # enable programmable completion features
 if [[ -f '/etc/bash_completion' ]] && ! shopt -oq posix; then
-      source '/etc/bash_completion'
+    source '/etc/bash_completion'
 fi
 
-if [[ -x "$(which brew)" ]] ; then
-    brew="$(brew --repository)"
-    if [[ -d "${brew}/etc/bash_completion.d" ]] && ! shopt -oq posix; then
-	source_all "${brew}/etc/bash_completion.d"
-    fi
-    unset brew
-fi
-
-# Go over all the possible completion scripts available.
+# Go over all the possible completion scripts locally available.
 source_all "${BASHRCD}/completion"
--- bash/bashrc.d.symlink/completion/Darwin.sh
+++ bash/bashrc.d.symlink/completion/Darwin.sh
@@ -3,12 +3,10 @@
 if [[ "$(uname -s)" == "Darwin" ]] ; then
   # Autocompletion for Homebrew.
   if [[ -x "$(which brew)" ]] ; then
-    brew="$(brew --repository)"
-    export PATH="${brew}/bin:${PATH}"
-    export LD_LIBRARY_PATH="${brew}/lib:${LD_LIBRARY_PATH}"
+    brew="$(brew --prefix)"
 
-    if [[ -d "${brew}/etc/bash_completion.d" ]] ; then
-      source_all "${brew}/etc/bash_completion.d"
+    if [[ -f "${brew}/etc/bash_completion" ]] ; then
+      source "${brew}/etc/bash_completion"
     fi
   fi
 fi
