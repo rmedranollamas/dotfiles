458d9e88349e8163e129710606409ff3c3745818
Ramón Medrano Llamas <m3drano@gmail.com>
2015-02-14 20:34:38 +0100

    Handier git completion for the aliases.

--- bash/bashrc.d.symlink/completion.sh
+++ bash/bashrc.d.symlink/completion.sh
@@ -9,3 +9,7 @@ fi
 
 # Go over all the possible completion scripts locally available.
 source_all "${BASHRCD}/completion"
+
+# Some handy overrides.
+complete -o bashdefault -o default -o nospace -F _git g 2>/dev/null \
+    || complete -o default -o nospace -F _git g
