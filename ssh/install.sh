8ff9b420f037e2c09dd40ff728f9bc71e76634b3
Ramón Medrano Llamas <m3drano@gmail.com>
2015-02-28 18:56:13 +0100

    Add GitHub key generation.

--- ssh/install.sh
+++ ssh/install.sh
@@ -2,9 +2,14 @@
 
 log="${DOTFILES_ROOT}/logs/ssh.install.log"
 bitbucket="${DOTFILES_ROOT}/ssh/ssh.symlink/bitbucket"
+bitbucket="${DOTFILES_ROOT}/ssh/ssh.symlink/github"
 
 if [[ ! -f "${bitbucket}" ]] ; then
   ssh-keygen -b 4096 -t rsa -N '' -C 'm3drano@gmail.com' -f "${bitbucket}" > "${log}" 2>&1
+fi
+
+if [[ ! -f "${github}" ]] ; then
+  ssh-keygen -b 4096 -t rsa -N '' -C 'm3drano+github@gmail.com' -f "${github}" > "${log}" 2>&1
 fi
 
 unset bitbucket
