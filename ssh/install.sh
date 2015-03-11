5a4c9ecb7b64d26437f1b0ffc5e519a663a7eef0
Ramón Medrano Llamas <m3drano@gmail.com>
2015-03-11 22:06:25 +0100

    Add SSH key generation for SourceForge.net.

--- ssh/install.sh
+++ ssh/install.sh
@@ -2,15 +2,25 @@
 
 log="${DOTFILES_ROOT}/logs/ssh.install.log"
 bitbucket="${DOTFILES_ROOT}/ssh/ssh.symlink/bitbucket"
-bitbucket="${DOTFILES_ROOT}/ssh/ssh.symlink/github"
+github="${DOTFILES_ROOT}/ssh/ssh.symlink/github"
+sourceforge="${DOTFILES_ROOT}/ssh/ssh.symlink/sourceforge"
 
 if [[ ! -f "${bitbucket}" ]] ; then
   ssh-keygen -b 4096 -t rsa -N '' -C 'm3drano@gmail.com' -f "${bitbucket}" > "${log}" 2>&1
+  chmod 400 "${bitbucket}*"
 fi
 
 if [[ ! -f "${github}" ]] ; then
-  ssh-keygen -b 4096 -t rsa -N '' -C 'm3drano+github@gmail.com' -f "${github}" > "${log}" 2>&1
+  ssh-keygen -b 4096 -t rsa -N '' -C 'm3drano+github@gmail.com' -f "${github}" >> "${log}" 2>&1
+  chmod 400 "${github}*"
 fi
 
+if [[ ! -f "${sourceforge}" ]] ; then
+  ssh-keygen -t dsa -N '' -C 'medranollamas@shell.sf.net' -f "${sourceforge}" >> "${log}" 2>&1
+  chmod 400 "${sourceforge}*"
+fi
+
+unset sourceforge
+unset github
 unset bitbucket
 unset log
