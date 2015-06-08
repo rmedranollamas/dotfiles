056ef099bf94af212eee7c19582b9c376dd4a5e5
Ramón Medrano Llamas <m3drano@gmail.com>
2015-06-08 14:11:51 +0200

    Cleanup the Python config.

--- emacs/emacs.d.symlink/config/python-config.el
+++ emacs/emacs.d.symlink/config/python-config.el
@@ -1,16 +1,11 @@
 ;; -*- mode: Lisp -*-
 ;; python-mode config
 
-;; Activate minor whitespace mode when in python mode.
-(add-hook 'python-mode-hook 'whitespace-mode)
+;; Load the virtualenvs.
+(require 'virtualenvwrapper)
+(venv-initialize-interactive-shells)
+(setq venv-location "/Users/m3drano/.virtualenvs")
 
-;; Add a shortcut for indenting and "out"denting.
-(global-set-key (kbd "C-.") 'python-indent-shift-right)
-(global-set-key (kbd "C-,") 'python-indent-shift-left)
-
-;; Configure flymake for Python
-(defvar python-pylint nil "Path to epylint")
-(setq python-pylint "/usr/local/bin/epylint")
 (when (load "flymake" t)
   (defun flymake-pylint-init ()
     (let* ((temp-file (flymake-init-create-temp-buffer-copy
@@ -18,15 +13,16 @@
            (local-file (file-relative-name
                         temp-file
                         (file-name-directory buffer-file-name))))
-      (list (expand-file-name python-pylint "") (list local-file))))
+      (list (expand-file-name "/usr/local/bin/epylint" "") (list local-file))))
   (add-to-list 'flymake-allowed-file-name-masks
                '("\\.py\\'" flymake-pylint-init)))
 
 ;; Django modes.
 (add-to-list 'auto-mode-alist '("\\.djhtml$" . django-html-mode))
 
-;; Load the virtualenvs.
-(require 'virtualenvwrapper)
-; (venv-initialize-interactive-shells)
-; (venv-initialize-eshell)
-(setq venv-location "~/.virtualenvs")
+;; Jedi for Python.
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:complete-on-dot t)
+
+;; Activate minor whitespace mode when in python mode.
+(add-hook 'python-mode-hook 'whitespace-mode)
