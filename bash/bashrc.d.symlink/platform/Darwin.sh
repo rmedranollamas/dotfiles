655918d48056c63fa73cdd3a58b4c73a22b55b90
Ramón Medrano Llamas <m3drano@gmail.com>
2015-06-09 12:52:25 +0200

    Android Studio for Mac CLI tools.

--- bash/bashrc.d.symlink/platform/Darwin.sh
+++ bash/bashrc.d.symlink/platform/Darwin.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 # -*- mode: sh -*-
 # Specifics for OS X.
+
+# Android Studio platform tools.
+ANDROID_HOME="${HOME}/Library/Android/sdk"
+export ANDROID_HOME
+
+PATH="${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools"
+export PATH
