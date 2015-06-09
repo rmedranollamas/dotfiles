6e3afcb3a67aa1c872a6e4e8a85099a75cd71d41
Ramón Medrano Llamas <m3drano@gmail.com>
2015-06-09 12:52:03 +0200

    Disable pip download cache flag.

--- bash/bashrc.d.symlink/python.sh
+++ bash/bashrc.d.symlink/python.sh
@@ -5,9 +5,6 @@
 # pip should only run if there is a virtualenv currently activated.
 export PIP_REQUIRE_VIRTUALENV='true'
 
-# Cache all packages globally.
-export PIP_DOWNLOAD_CACHE="${HOME}/.pip/cache"
-
 # Configuration for the Python interpreter.
 export PYTHONSTARTUP="${HOME}/.pythonrc"
 
