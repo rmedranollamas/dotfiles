ff2ffa8214da2e152c6339ee04637a3e8dffce15
Ramón Medrano Llamas <m3drano@gmail.com>
2015-06-09 12:52:43 +0200

    Remove non used gcloud bash config.

--- bash/bashrc.d.symlink/other.sh
+++ bash/bashrc.d.symlink/other.sh
@@ -5,11 +5,6 @@
 # Easier cd for the Home folders.
 CDPATH=':~'
 
-# Look for the Cloud SDK config file.
-if [[ -f "${HOME}/.bashrc_gcloud" ]] ; then
-  source "${HOME}/.bashrc_gcloud"
-fi
-
 # Export the GPG_TTY variable.
 GPG_TTY="$(tty)"
 export GPG_TTY
