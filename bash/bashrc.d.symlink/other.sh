a545a572f509e01a44d931a421870c2d755ab836
Ramón Medrano Llamas <m3drano@gmail.com>
2015-08-03 22:14:26 +0200

    Export the GPG_AGENT_INFO env.

--- bash/bashrc.d.symlink/other.sh
+++ bash/bashrc.d.symlink/other.sh
@@ -5,6 +5,13 @@
 # Easier cd for the Home folders.
 CDPATH=':~'
 
-# Export the GPG_TTY variable.
+# Export the GPG_TTY variable and the agent location.
+if [[ "$(uname -s)" == 'Darwin' ]]; then
+  if [ -f "${HOME}/.gpg-agent-info" ]; then
+    source  "${HOME}/.gpg-agent-info"
+    export GPG_AGENT_INFO
+  fi
+fi
+
 GPG_TTY="$(tty)"
 export GPG_TTY
