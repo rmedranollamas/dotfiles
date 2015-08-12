5d978e128616ea0b1c0107dd9556c07627049d82
Ramón Medrano Llamas <m3drano+bitbucket@gmail.com>
2015-08-12 09:52:29 +0000

    Remove key generation for GitHub.

--- ssh/install.sh
+++ ssh/install.sh
@@ -2,17 +2,11 @@
 
 log="${DOTFILES_ROOT}/logs/ssh.install.log"
 bitbucket="${DOTFILES_ROOT}/ssh/ssh.symlink/bitbucket"
-github="${DOTFILES_ROOT}/ssh/ssh.symlink/github"
 sourceforge="${DOTFILES_ROOT}/ssh/ssh.symlink/sourceforge"
 
 if [[ ! -f "${bitbucket}" ]] ; then
   ssh-keygen -b 4096 -t rsa -N '' -C 'm3drano@gmail.com' -f "${bitbucket}" > "${log}" 2>&1
   chmod 400 "${bitbucket}*"
-fi
-
-if [[ ! -f "${github}" ]] ; then
-  ssh-keygen -b 4096 -t rsa -N '' -C 'm3drano+github@gmail.com' -f "${github}" >> "${log}" 2>&1
-  chmod 400 "${github}*"
 fi
 
 if [[ ! -f "${sourceforge}" ]] ; then
@@ -21,6 +15,5 @@ if [[ ! -f "${sourceforge}" ]] ; then
 fi
 
 unset sourceforge
-unset github
 unset bitbucket
 unset log
