8fb5e5324fd952197567b7b0afebf4343e1834cb
Ramón Medrano Llamas <m3drano+bitbucket@gmail.com>
2015-08-12 11:05:57 +0000

    Use emacs-nox instead of emacs24 in Linux.

--- os/Linux/install.sh
+++ os/Linux/install.sh
@@ -4,7 +4,7 @@ log="${DOTFILES_ROOT}/logs/os.install.log"
 touch "${log}"
 
 # Installs packages in Ubuntu.
-packages='tmux emacs24 colordiff python-pip python-virtualenv ipython mosh'
+packages='tmux emacs-nox colordiff python-pip python-virtualenv ipython mosh'
 sudo -n sh -c "apt-get install -q -y ${packages} > ${log} 2>&1"
 
 unset log
