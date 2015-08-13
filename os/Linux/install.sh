8b14a9885ce2ee547fdae428e0788d7cd7122a64
Ramón Medrano Llamas <m3drano+bitbucket@gmail.com>
2015-08-13 15:32:34 +0000

    Install Python 3 in Debian.

--- os/Linux/install.sh
+++ os/Linux/install.sh
@@ -4,7 +4,7 @@ log="${DOTFILES_ROOT}/logs/os.install.log"
 touch "${log}"
 
 # Installs packages in Ubuntu.
-packages='tmux emacs-nox colordiff python-pip python-virtualenv ipython mosh'
+packages='tmux emacs-nox colordiff python3 python3-pip python3-virtualenv'
 sudo -n sh -c "apt-get install -q -y ${packages} > ${log} 2>&1"
 
 unset log
