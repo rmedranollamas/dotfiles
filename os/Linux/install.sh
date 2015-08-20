a9a4ef4045e416a0ecf5170f8b60391e2d61afe9
Ramón Medrano Llamas <m3drano+bitbucket@gmail.com>
2015-08-20 17:55:41 +0000

    Add virtualenv package to Debian install config.

--- os/Linux/install.sh
+++ os/Linux/install.sh
@@ -3,8 +3,8 @@
 log="${DOTFILES_ROOT}/logs/os.install.log"
 touch "${log}"
 
-# Installs packages in Ubuntu.
-packages='tmux emacs-nox colordiff python3 python3-pip python3-virtualenv'
+# Installs packages in Debian.
+packages='tmux emacs-nox colordiff python3 python3-pip python3-virtualenv virtualenv'
 sudo -n sh -c "apt-get install -q -y ${packages} > ${log} 2>&1"
 
 unset log
