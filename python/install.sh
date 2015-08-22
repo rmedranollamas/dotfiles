f68ed79aadaa1eb50f4ff12b13144fedf15b074c
Ramón Medrano Llamas <m3drano+bitbucket@gmail.com>
2015-08-22 20:45:57 +0000

    Do not install ipython.

--- python/install.sh
+++ python/install.sh
@@ -5,7 +5,7 @@ log="${DOTFILES_ROOT}/logs/python.install.log"
 # On Linux, we prefer always standard packages.
 if [[ "$(uname -s)" == "Darwin" ]] ; then
   # Install some system-wide Python tools.
-  PIP_REQUIRE_VIRTUALENV='' pip install --upgrade ipython virtualenv > "${log}" 2>&1
+  PIP_REQUIRE_VIRTUALENV='' pip install --upgrade pip virtualenv > "${log}" 2>&1
 fi
 
 unset log
