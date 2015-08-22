6249072260517b045f4b34887261016c91f3bb55
Ramón Medrano Llamas <m3drano+bitbucket@gmail.com>
2015-08-22 19:47:35 +0000

    Compact the OS X brew completion script.

--- bash/bashrc.d.symlink/completion/Darwin.sh
+++ bash/bashrc.d.symlink/completion/Darwin.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 if [[ "$(uname -s)" == "Darwin" ]] ; then
-  # Autocompletion for Homebrew.
   if [[ -x "$(which brew)" ]] ; then
     brew="$(brew --prefix)"
-
     if [[ -f "${brew}/etc/bash_completion" ]] ; then
       source "${brew}/etc/bash_completion"
     fi
+    unset brew
   fi
 fi
-
-unset brew
