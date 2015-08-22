544e5a26a951d580f6900bb1176fb98c04b5c466
Ramón Medrano Llamas <m3drano+bitbucket@gmail.com>
2015-08-22 19:51:55 +0000

    Make platform/Darwin.sh platform safe.

--- bash/bashrc.d.symlink/platform/Darwin.sh
+++ bash/bashrc.d.symlink/platform/Darwin.sh
@@ -2,9 +2,9 @@
 # -*- mode: sh -*-
 # Specifics for OS X.
 
-# Android Studio platform tools.
-ANDROID_HOME="${HOME}/Library/Android/sdk"
-export ANDROID_HOME
-
-PATH="${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools"
-export PATH
+if [[ "$(uname -s)" == "Darwin" ]] ; then
+  if [[ -d "${HOME}/Library/Android/sdk" ]] ; then
+    export ANDROID_HOME="${HOME}/Library/Android/sdk"
+    export PATH="${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools"
+  fi
+fi
