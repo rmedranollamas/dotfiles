fd3fc9f251edf6a0bb8c47f08245ec12a7b114fc
Ramón Medrano Llamas <m3drano+bitbucket@gmail.com>
2015-09-03 17:58:16 +0000

    Secure ssh keys for SourceForge too.

--- ssh/install.sh
+++ ssh/install.sh
@@ -5,12 +5,12 @@ bitbucket="${DOTFILES_ROOT}/ssh/ssh.symlink/bitbucket"
 sourceforge="${DOTFILES_ROOT}/ssh/ssh.symlink/sourceforge"
 
 if [[ ! -f "${bitbucket}" ]] ; then
-  ssh-keygen -a 1000 -o -b 4096 -t rsa -N '' -C 'm3drano@gmail.com' -f "${bitbucket}" > "${log}" 2>&1
+  ssh-keygen -a 100 -o -b 4096 -t rsa -N '' -C 'm3drano@gmail.com' -f "${bitbucket}" > "${log}" 2>&1
   chmod 400 "${bitbucket}*"
 fi
 
 if [[ ! -f "${sourceforge}" ]] ; then
-  ssh-keygen -t dsa -N '' -C 'medranollamas@shell.sf.net' -f "${sourceforge}" >> "${log}" 2>&1
+  ssh-keygen -a 100 -o -b 4096 -t rsa -N '' -C 'medranollamas@shell.sf.net' -f "${sourceforge}" >> "${log}" 2>&1
   chmod 400 "${sourceforge}*"
 fi
 
