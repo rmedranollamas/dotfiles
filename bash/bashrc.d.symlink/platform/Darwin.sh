2a6d377045605e7d1e53588195aec9b04e519244
Ramón Medrano Llamas <m3drano@gmail.com>
2015-09-20 12:35:57 +0200

    Add /usr/local/sbin to the path.

--- bash/bashrc.d.symlink/platform/Darwin.sh
+++ bash/bashrc.d.symlink/platform/Darwin.sh
@@ -5,6 +5,10 @@
 if [[ "$(uname -s)" == "Darwin" ]] ; then
   if [[ -d "${HOME}/Library/Android/sdk" ]] ; then
     export ANDROID_HOME="${HOME}/Library/Android/sdk"
-    export PATH="${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools"
+    export PATH="${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools:${PATH}"
   fi
+fi
+
+if [[ -d '/usr/local/sbin' ]] ; then
+    export PATH="/usr/local/sbin:${PATH}"
 fi
