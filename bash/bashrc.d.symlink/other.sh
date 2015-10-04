e99c54582bc897d68268da5308ae93cff9e12411
Ramón Medrano Llamas <m3drano@gmail.com>
2015-10-04 16:00:51 +0200

    Remove the crap of gpg-agent of Homebrew.

--- bash/bashrc.d.symlink/other.sh
+++ bash/bashrc.d.symlink/other.sh
@@ -5,10 +5,4 @@
 # Easier cd for the Home folders.
 CDPATH=':~'
 
-# Export the GPG_TTY variable and the agent location.
-if [ -f "${HOME}/.gpg-agent-info" ]; then
-  source  "${HOME}/.gpg-agent-info"
-  export GPG_AGENT_INFO
-fi
-
 export GPG_TTY="$(tty)"
