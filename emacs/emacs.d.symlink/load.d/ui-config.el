c6f80731be76268d87168e019fccaf276e25a67d
Ramón Medrano Llamas <m3drano@gmail.com>
2015-11-25 22:32:06 +0100

    Load light solarized on X11.

--- emacs/emacs.d.symlink/load.d/ui-config.el
+++ emacs/emacs.d.symlink/load.d/ui-config.el
@@ -18,7 +18,12 @@
  '(solarized-termcolors 256)
  '(solarized-broken-srgb t)
  '(frame-background-mode 'dark))
-(enable-theme 'solarized)
+(add-hook 'after-make-frame-functions
+          (lambda (frame)
+            (let ((mode (if (display-graphic-p frame) 'light 'dark)))
+              (set-frame-parameter frame 'background-mode mode)
+              (set-terminal-parameter frame 'background-mode mode))
+            (enable-theme 'solarized)))
 
 ;; Indentation.
 (setq-default tab-width 2)
