94ff0be464aad83b9f3f108aa4bee96a4fa919b9
Ramón Medrano Llamas <m3drano@gmail.com>
2016-01-01 19:58:00 +0100

    rfringe only in GUI mode.

--- emacs/emacs.d.symlink/load.d/flymake-config.el
+++ emacs/emacs.d.symlink/load.d/flymake-config.el
@@ -13,7 +13,9 @@
 (eval-after-load 'flymake '(require 'flymake-cursor))
 
 ;; Show errors on the right fringe.
+(cond (window-system (progn
 (eval-after-load 'flymake '(require 'rfringe))
+)))
 
 ;; Also enable auto-complete.
 (require 'auto-complete)
