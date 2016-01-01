089db5b0205183de14a061ca9061c2422c0e1369
Ramón Medrano Llamas <m3drano@gmail.com>
2016-01-01 19:37:54 +0100

    Tweak the Python/virtualenv configuration.

--- bash/bashrc.d.symlink/python.sh
+++ bash/bashrc.d.symlink/python.sh
@@ -2,18 +2,15 @@
 # -*- mode:sh -*-
 # Python REPL configuration.
 
-# pip should only run if there is a virtualenv currently activated.
-export PIP_REQUIRE_VIRTUALENV='true'
-
 # Configuration for the Python interpreter.
 export PYTHONSTARTUP="${HOME}/.pythonrc"
 
-# virtualenvwrapper config.
+# virtualenvwrapper and virtualenv.
+export VIRTUALENV_PYTHON="$(which python)"
+export VIRTUALENVWRAPPER_PYTHON="${VIRTUALENV_PYTHON}"
+
+# virtualenvwrapper config for MacPorts.
 if [[ -f '/opt/local/bin/virtualenvwrapper_lazy.sh-3.4' ]] ; then
-  export WORKON_HOME="${HOME}/.virtualenvs"
-  export PROJECT_HOME="${HOME}/Code"
-  export VIRTUALENV_PYTHON="$(which python3)"
-  export VIRTUALENVWRAPPER_PYTHON="${VIRTUALENV_PYTHON}"
   export VIRTUALENVWRAPPER_SCRIPT='/opt/local/bin/virtualenvwrapper.sh-3.4'
   source '/opt/local/bin/virtualenvwrapper_lazy.sh-3.4'
 fi
