7221454750ff3681bc4ae2ff67cbca3f923bdff6
Ramón Medrano Llamas <m3drano@gmail.com>
2016-03-17 21:31:05 +0100

    Back to Homebrew :(

--- bash/bashrc.d.symlink/platform/Darwin.sh
+++ bash/bashrc.d.symlink/platform/Darwin.sh
@@ -10,18 +10,6 @@ if [[ "$(uname -s)" == "Darwin" ]] ; then
     export PATH="${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools:${PATH}"
   fi
 
-  if [[ -d '/opt/local/sbin' ]] ; then
-    export PATH="/opt/local/sbin:${PATH}"
-  fi
-
-  if [[ -d '/opt/local/bin' ]] ; then
-    export PATH="/opt/local/bin:${PATH}"
-  fi
-
-  if [[ -d '/opt/local/share/man' ]] ; then
-    export MANPATH="/opt/local/share/man:${MANPATH}"
-  fi
-
   if [[ -d '/Applications/Emacs.app/Contents/MacOS/bin' ]] ; then
     export PATH="/Applications/Emacs.app/Contents/MacOS/bin:${PATH}"
     export PATH="/Applications/Emacs.app/Contents/MacOS:${PATH}"
