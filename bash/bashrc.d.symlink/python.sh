99013295d4c75ceb036bf436e5a18da5b4f1c5f7
Ramón Medrano Llamas <m3drano@gmail.com>
2016-03-17 22:08:56 +0100

    Use Homebrew's virtualenv.

--- bash/bashrc.d.symlink/python.sh
+++ bash/bashrc.d.symlink/python.sh
@@ -6,12 +6,13 @@
 export PYTHONSTARTUP="${HOME}/.pythonrc"
 
 # virtualenvwrapper and virtualenv.
-export VIRTUALENV_PYTHON="$(which python)"
+export PIP_REQUIRE_VIRTUALENV='true'
+export VIRTUALENV_PYTHON="/usr/local/bin/python3"
 export VIRTUALENVWRAPPER_PYTHON="${VIRTUALENV_PYTHON}"
 export WORKON_HOME="${HOME}/Code/.virtualenvs"
 
-# virtualenvwrapper config for MacPorts.
-if [[ -f '/opt/local/bin/virtualenvwrapper_lazy.sh-3.4' ]] ; then
-  export VIRTUALENVWRAPPER_SCRIPT='/opt/local/bin/virtualenvwrapper.sh-3.4'
-  source '/opt/local/bin/virtualenvwrapper_lazy.sh-3.4'
+# virtualenvwrapper config for Homebrew.
+if [[ -f `which virtualenvwrapper_lazy.sh` ]] ; then
+  export VIRTUALENVWRAPPER_SCRIPT=`which virtualenvwrapper_lazy.sh`
+  source `which virtualenvwrapper_lazy.sh`
 fi
