9d0642d9d583f54b71c4356afe9b3a345cfa728c
Ramón Medrano Llamas <m3drano@gmail.com>
2016-06-11 14:59:27 +0200

    Remove duplicate.

--- emacs/emacs.d.symlink/load.d/go-config.el
+++ emacs/emacs.d.symlink/load.d/go-config.el
@@ -3,10 +3,6 @@
 
 (require 'go-mode-autoloads)
 
-;; Load flymake-go when using go-mode.
-(eval-after-load "go-mode"
-  '(require 'flymake-go))
-
 ;; Do not show the tabs as tabs, which is horrendous.
 (add-hook 'go-mode-hook (lambda () (setq tab-width 2)))
 
