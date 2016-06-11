ddafe81ab26a119309a29e8402bb4ae0f492052f
Ramón Medrano Llamas <m3drano@gmail.com>
2016-06-11 15:23:37 +0200

    Do not always load flymake for real.

--- emacs/emacs.d.symlink/load.d/flymake-config.el
+++ emacs/emacs.d.symlink/load.d/flymake-config.el
@@ -1,14 +1,8 @@
 ;;; flymake-config.el --- Configs for Flymake et al.
 ;; -*- mode: Emacs-Lisp -*-
 
-;; Enable Flymake by default.
-(require 'flymake)
-
 ;; Flymake temp files not inplace.
 (setq temporary-file-directory "~/.emacs.d/tmp/")
-
-;; Enable Flymake on every file load.
-(add-hook 'find-file-hook 'flymake-find-file-hook)
 
 ;; Show details of errors on the minubuffer.
 (eval-after-load 'flymake '(require 'flymake-cursor))
