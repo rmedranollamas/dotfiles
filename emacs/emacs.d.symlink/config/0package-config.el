8b88c08d9f0306b7326eea82496cec7430a0b230
Ramón Medrano Llamas <m3drano@gmail.com>
2016-07-16 12:18:36 +0200

    Remove marmalade (is just so very old).

--- emacs/emacs.d.symlink/config/0package-config.el
+++ emacs/emacs.d.symlink/config/0package-config.el
@@ -21,10 +21,6 @@
                      virtualenvwrapper))
 
 (add-to-list 'package-archives
-             '("marmalade" .
-               "https://marmalade-repo.org/packages/") t)
-
-(add-to-list 'package-archives
              '("melpa" .
                "https://melpa.org/packages/") t)
 
