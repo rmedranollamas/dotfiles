d400d32178ca966f750a0ead13a04f4b3ba3043d
Ramón Medrano Llamas <m3drano@gmail.com>
2016-10-18 11:18:39 +0200

    GitHub ssh key generation is back.

--- ssh/install.sh
+++ ssh/install.sh
@@ -2,12 +2,18 @@
 
 log="${DOTFILES_ROOT}/logs/ssh.install.log"
 bitbucket="${DOTFILES_ROOT}/ssh/ssh.symlink/bitbucket"
+github="${DOTFILES_ROOT}/ssh/ssh.symlink/github"
 sourceforge="${DOTFILES_ROOT}/ssh/ssh.symlink/sourceforge"
 gce="${DOTFILES_ROOT}/ssh/ssh.symlink/google_compute_engine"
 
 if [[ ! -f "${bitbucket}" ]] ; then
   ssh-keygen -a 100 -o -b 4096 -t rsa -N '' -C 'm3drano@gmail.com' -f "${bitbucket}" > "${log}" 2>&1
   chmod 400 "${bitbucket}*"
+fi
+
+if [[ ! -f "${github}" ]] ; then
+  ssh-keygen -a 100 -o -t ed25519 -N '' -C "m3drano@$(hostname -f)" -f "${github}" >> "${log}" 2>&1
+  chmod 400 "${github}*"
 fi
 
 if [[ ! -f "${sourceforge}" ]] ; then
@@ -22,5 +28,6 @@ fi
 
 unset gce
 unset sourceforge
+unset github
 unset bitbucket
 unset log
