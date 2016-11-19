db8ed7f5d2c34044eba9bb4cb221dfed4900d12e
Ramón Medrano Llamas <m3drano@gmail.com>
2016-11-19 23:09:25 +0100

    Update hterm preferences.

--- misc/secure_shell.js
+++ misc/secure_shell.js
@@ -1,25 +1,18 @@
 {
-    "magic": "nassh-prefs",
-    "version": 1,
-    "nassh": {
-        "profile-ids": []
-    },
     "hterm": {
         "default": {
             "alt-sends-what": "browser-key",
             "background-color": "#002b36",
-            "cursor-blink": true,
-            "cursor-color": "#ff0000",
             "color-palette-overrides": [
                 "#073642",
                 "#dc322f",
-                "#859900", 
+                "#859900",
                 "#b58900",
                 "#268bd2",
-                null,
+                "#d33682",
                 "#2aa198",
                 "#eee8d5",
-                "#fdf6e3",
+                "#002b36",
                 "#cb4b16",
                 "#586e75",
                 "#657b83",
@@ -28,7 +21,10 @@
                 "#93a1a1",
                 "#fdf6e3"
             ],
+            "copy-on-select": false,
             "ctrl-plus-minus-zero-zoom": false,
+            "cursor-blink": true,
+            "cursor-color": "rgba(255, 0, 0, 1)",
             "enable-8-bit-control": true,
             "enable-bold": true,
             "enable-bold-as-bright": false,
@@ -37,14 +33,49 @@
                 "TERM": "xterm-256color"
             },
             "font-family": "\"Inconsolata\", monospace",
-            "font-size": 12,
+            "font-size": 14,
             "font-smoothing": "subpixel-antialiased",
             "foreground-color": "#839496",
             "media-keys-are-fkeys": true,
-            "mouse-paste-button": true,
             "scrollbar-visible": false,
             "shift-insert-paste": false,
             "user-css": "https://fonts.googleapis.com/css?family=Inconsolata"
         }
-    }
+    },
+    "magic": "nassh-prefs",
+    "nassh": {
+        "profile-ids": [
+            {
+                "id": "bddb",
+                "json": {
+                    "description": "niobium@jarvis.zrh",
+                    "hostname": "jarvis.zrh.corp.google.com",
+                    "port": "",
+                    "relay-options": "--config=google",
+                    "username": "niobium"
+                }
+            },
+            {
+                "id": "89e7",
+                "json": {
+                    "description": "niobium@badulla",
+                    "hostname": "badulla.i.corp.google.com",
+                    "port": "",
+                    "relay-options": "--config=google",
+                    "username": "niobium"
+                }
+            },
+            {
+                "id": "3f9b",
+                "json": {
+                    "description": "niobium@wkmcp (emergency)",
+                    "hostname": "wkmcp.prodz",
+                    "port": "",
+                    "relay-options": "--config=google --no-proxy-host",
+                    "username": "niobium"
+                }
+            }
+        ]
+    },
+    "version": 1
 }
