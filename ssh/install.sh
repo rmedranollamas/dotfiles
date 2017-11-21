2caa15472fe8c9ca6384b65584727f5b378e59d5
Ramón Medrano Llamas <m3drano@gmail.com>
2017-11-21 20:06:56 +0100

    Do not generate keys for unused services.

--- ssh/install.sh
+++ ssh/install.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
 log="${DOTFILES_ROOT}/logs/ssh.install.log"
-bitbucket="${DOTFILES_ROOT}/ssh/ssh.symlink/bitbucket"
 github="${DOTFILES_ROOT}/ssh/ssh.symlink/github"
 sourceforge="${DOTFILES_ROOT}/ssh/ssh.symlink/sourceforge"
-gce="${DOTFILES_ROOT}/ssh/ssh.symlink/google_compute_engine"
-
-if [[ ! -f "${bitbucket}" ]] ; then
-  ssh-keygen -a 100 -o -t ed25519 -N '' -C 'm3drano@gmail.com' -f "${bitbucket}" >> "${log}" 2>&1
-  chmod 400 "${bitbucket}*"
-fi
 
 if [[ ! -f "${github}" ]] ; then
   ssh-keygen -a 100 -o -t ed25519 -N '' -C "m3drano@$(hostname -f)" -f "${github}" >> "${log}" 2>&1
@@ -21,13 +14,6 @@ if [[ ! -f "${sourceforge}" ]] ; then
   chmod 400 "${sourceforge}*"
 fi
 
-if [[ ! -f "${gce}" ]] ; then
-  ssh-keygen -a 100 -o -t ed25519 -N '' -C "m3drano@$(hostname -f)" -f "${gce}" >> "${log}" 2>&1
-  chmod 400 "${gce}*"
-fi
-
-unset gce
 unset sourceforge
 unset github
-unset bitbucket
 unset log
