b0a8ec6f0a6d8ea6965392deabd8d02402211e9d
Ramón Medrano Llamas <m3drano@gmail.com>
2017-12-12 22:52:55 +0100

    Bitbucket SSH key generation is back.

--- ssh/install.sh
+++ ssh/install.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 log="${DOTFILES_ROOT}/logs/ssh.install.log"
+bitbucket="${DOTFILES_ROOT}/ssh/ssh.symlink/bitbucket"
 github="${DOTFILES_ROOT}/ssh/ssh.symlink/github"
 sourceforge="${DOTFILES_ROOT}/ssh/ssh.symlink/sourceforge"
+
+if [[ ! -f "${bitbucket}" ]] ; then
+  ssh-keygen -a 100 -o -t ed25519 -N '' -C "m3drano@$(hostname -f)" -f "${bitbucket}" >> "${log}" 2>&1
+  chmod 400 "${bitbucket}*"
+fi
 
 if [[ ! -f "${github}" ]] ; then
   ssh-keygen -a 100 -o -t ed25519 -N '' -C "m3drano@$(hostname -f)" -f "${github}" >> "${log}" 2>&1
@@ -16,4 +22,5 @@ fi
 
 unset sourceforge
 unset github
+unset bitbucket
 unset log
