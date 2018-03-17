0899965f95fc6793f072206bfb1aaf5c4cf97697
Ramón Medrano Llamas <m3drano@gmail.com>
2018-03-17 22:58:22 +0100

    Do not export hist control variables.

--- bash/bashrc.d.symlink/history.sh
+++ bash/bashrc.d.symlink/history.sh
@@ -8,7 +8,7 @@ shopt -s histappend
 
 # Don't put duplicate lines or lines starting with spaces in the history and
 # use a sane date format.
-export HISTCONTROL='ignoreboth'
-export HISTTIMEFORMAT='%d/%m/%Y %T - '
-export HISTSIZE=1000000
-export HISTFILESIZE=1000000000
+HISTCONTROL='ignoreboth'
+HISTTIMEFORMAT='%d/%m/%Y %T - '
+HISTSIZE=1000000
+HISTFILESIZE=1000000000
