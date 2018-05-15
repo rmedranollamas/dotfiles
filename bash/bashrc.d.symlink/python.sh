5cd088559c5afe0550d5fb38e1b3e481eb9c9dba
Ramón Medrano Llamas <m3drano@gmail.com>
2018-05-15 22:25:33 +0200

    pyenv is gone.

--- bash/bashrc.d.symlink/python.sh
+++ bash/bashrc.d.symlink/python.sh
@@ -3,10 +3,3 @@
 # Python REPL configuration.
 
 export PYTHONSTARTUP="${HOME}/.pythonrc"
-export PYENV_ROOT="${HOME}/.pyenv"
-export PATH="${PYENV_ROOT}/bin:${PATH}"
-export PYENV_VIRTUALENV_DISABLE_PROMPT=1
-export WORKON_HOME="${HOME}/.pyenv/versions"
-
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
