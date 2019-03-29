a1ba70a1a5c0e7a4e1daefcee8f79b1be26f2c8c
Ramón Medrano Llamas <m3drano@gmail.com>
2019-03-29 15:00:10 +0100

    Some nits for Emacs on macOS getting shell arguments.

--- emacs/emacs.d.symlink/load.d/darwin-config.el
+++ emacs/emacs.d.symlink/load.d/darwin-config.el
@@ -4,6 +4,9 @@
 (if (eq system-type 'darwin)
     (progn
       (when (memq window-system '(mac ns x))
+        (require 'exec-path-from-shell)
+        (setq exec-path-from-shell-arguments '("-l"))
+        (add-to-list 'exec-path-from-shell-variables "LC_ALL")
         (exec-path-from-shell-initialize))
       ;; Use Spotlight for search files.
       (defvar locate-command "mdfind"))
