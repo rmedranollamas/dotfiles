9f7d1911ccef9d889e2d1489b48af9086961916e
Ramón Medrano Llamas <m3drano@gmail.com>
2019-03-29 14:56:47 +0100

    Renable SyncTeX, even though only macOS.

--- emacs/emacs.d.symlink/load.d/latex-config.el
+++ emacs/emacs.d.symlink/load.d/latex-config.el
@@ -17,10 +17,31 @@
 (setq TeX-command-default "LatexMk")
 
 ;; Always use PDF mode.
-(defvar TeX-PDF-mode nil "PDFmode for TeX")
 (setq TeX-PDF-mode t)
 
 ;; Enable flyspell automatically when loading a TeX file.
-(add-hook 'LaTeX-mode-hook #'flyspell-mode)
+(add-hook 'LaTeX-mode-hook 'turn-on-flyspell)
 (defvar ispell-extra-args nil "extra args for ispell")
 (setq ispell-extra-args '("--sug-mode=fast"))
+
+;; SyncTeX
+(setq TeX-view-program-selection '((output-pdf "Skim")))
+(add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
+(setq TeX-source-correlate-method 'synctex)
+
+(add-hook 'LaTeX-mode-hook
+          (lambda()
+            (add-to-list 'TeX-expand-list
+                         '("%q" skim-make-url))))
+
+(defun skim-make-url () (concat
+                         (TeX-current-line)
+                         " "
+                         (expand-file-name (funcall file (TeX-output-extension) t)
+                                           (file-name-directory (TeX-master-file)))
+                         " "
+                         (buffer-file-name)
+                         ))
+
+(setq TeX-view-program-list
+      '(("Skim" "/Applications/Skim.app/Contents/SharedSupport/displayline %q")))
