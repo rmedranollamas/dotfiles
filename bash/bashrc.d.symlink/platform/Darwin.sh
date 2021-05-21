949a548a61fe821929da869dc92152fa4927df73
Ramón Medrano Llamas <ramon.medrano.llamas@icloud.com>
2021-05-21 16:23:37 +0200

    Cleanup macOS config.

--- bash/bashrc.d.symlink/platform/Darwin.sh
+++ bash/bashrc.d.symlink/platform/Darwin.sh
@@ -8,18 +8,10 @@ if [[ "$(uname -s)" == "Darwin" ]] ; then
   export HOMEBREW_NO_ANALYTICS=1
   export HOMEBREW_NO_AUTO_UPDATE=1
   export HOMEBREW_NO_EMOJI=1
+  export OPENBLAS="$(brew --prefix openblas)"
 
   if brew command command-not-found-init > /dev/null 2>&1; then
     eval "$(brew command-not-found-init)"
-  fi
-
-  if [[ -d "${HOME}/Library/Android/sdk" ]] ; then
-    export ANDROID_HOME="${HOME}/Library/Android/sdk"
-    export PATH="${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools"
-  fi
-
-  if [[ -d "/usr/local/opt/go/libexec/bin" ]] ; then
-    export PATH="${PATH}:/usr/local/opt/go/libexec/bin"
   fi
 
   if [[ -f "${HOME}/.gnupg/gpg-agent-info" ]]; then
