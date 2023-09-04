427a6d0656173fabcb8a9b4b1815f1fb705745fe
Ramón Medrano Llamas <m3drano@gmail.com>
2023-09-04 22:35:38 +0200

    Refactor macOS bash config.

--- bash/bashrc.d.symlink/platform/Darwin.sh
+++ bash/bashrc.d.symlink/platform/Darwin.sh
@@ -3,12 +3,21 @@
 # Specifics for OS X.
 
 if [[ "$(uname -s)" == "Darwin" ]] ; then
-  export PATH="/usr/local/sbin:${PATH}"
+  export PATH="/usr/local/sbin${PATH+:$PATH}"
   export DISPLAY=':0.0'
-  export HOMEBREW_NO_ANALYTICS=1
-  export HOMEBREW_NO_AUTO_UPDATE=1
-  export HOMEBREW_NO_EMOJI=1
-  export HOMEBREW_NO_INSTALL_CLEANUP=1
+
+  if [[ -x "/usr/local/bin/brew" ]]; then
+    eval "$(/usr/local/bin/brew shellenv)"
+    export HOMEBREW_NO_ANALYTICS=1
+    export HOMEBREW_NO_AUTO_UPDATE=1
+    export HOMEBREW_NO_EMOJI=1
+    export HOMEBREW_NO_INSTALL_CLEANUP=1
+  fi
+
+  if [[ -d "/Applications/Emacs.app/Contents/MacOS/bin" ]]; then
+    export PATH="/Applications/Emacs.app/Contents/MacOS/bin${PATH+:$PATH}";
+    alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs.sh'
+  fi
 
   if [[ -f "${HOME}/.gnupg/gpg-agent-info" ]]; then
     source "${HOME}/.gnupg/gpg-agent-info"
