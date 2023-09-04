00b8795b99b24905f898f37faa4331833c931e37
Ramón Medrano Llamas <m3drano@gmail.com>
2023-09-04 22:13:58 +0200

    Super keys in macOS for Emacs.

--- emacs/emacs.d.symlink/load.d/darwin-config.el
+++ emacs/emacs.d.symlink/load.d/darwin-config.el
@@ -10,4 +10,13 @@
         (exec-path-from-shell-initialize))
       ;; Use Spotlight for search files.
       (defvar locate-command "mdfind"))
+
+  (global-set-key [(hyper a)] 'mark-whole-buffer)
+  (global-set-key [(hyper v)] 'yank)
+  (global-set-key [(hyper c)] 'kill-ring-save)
+  (global-set-key [(hyper s)] 'save-buffer)
+  (global-set-key [(hyper l)] 'goto-line)
+  (global-set-key [(hyper w)]
+                  (lambda () (interactive) (delete-window)))
+  (global-set-key [(hyper z)] 'undo)
 )
